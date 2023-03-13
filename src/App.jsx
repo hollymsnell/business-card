cfb293c940505d0c450ba96e3f83ff5efa74fa31
hollymsnell <hollymsnell@gmail.com>
2023-03-13 10:47:01 -0600

    updated business card

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+    <div className="container">
+      <img src="public/pic02.jpg" className="profile-photo" width="250" height="250" />
+      <h1>Holly Snell</h1>
+      <h4>Full-Stack Developer</h4>
+      <h3>About</h3>
+      <small>
+        Background in healthcare and human wellness. 2022 Bootcamp grad looking to become a software engineer.
+      </small>
+      <h3>Interests</h3>
+      <small>
+        Loves spending time being active outside when the weather is good. Including running, biking, hiking, kayaking,
+        paddle boarding, rock climbing, and more!
+      </small>
+      <footer>
+        <a href="https://github.com/hollymsnell" className="fa fa-github"></a>
+        <a href="https://www.linkedin.com/hollymsnell" className="fa fa-linkedin"></a>
+        <a href="mailto:hollymsnell@gmail.com" className="fa fa-envelope"></a>
+        <a href="https://hollymsnell.github.io" className="fa fa-heart"></a>
+      </footer>
+    </div>
+    </div>
+  )
+}
+
+export default App
